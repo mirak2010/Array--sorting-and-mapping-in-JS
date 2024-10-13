dd935e9ccdfbeafa58059bf5b65b65562b05e71d
mirak2010 <mirakhmad0101@yahoo.com>
2024-10-13 12:18:30 +0500

    #

--- eval.js
+++ eval.js
@@ -1,10 +1,9 @@
 function myfunction(){
     var x=10;
     var y=9;
-    var a= eval("x + y") + "<br>";
-    var b = eval("y - x") + "<br>";
-    var c = eval("2 * 8") + "<br>";
+    var a=eval("x-y") + "<br>"
+    var b=eval("y-x") + "<br>"
 
-    var res= a + b + c;
+    var res= a + b;
     document.getElementById("demo").innerHTML=res;
 }
